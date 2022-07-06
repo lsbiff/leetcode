4d08eaa419c2ebab141902858f788fe19488aef6
unknown <lucas.biff89@gmail.com>
2022-07-06 14:25:20 -0300

    cleaning code

--- 21.Merge Two Sorted Linked Lists.py
+++ 21.Merge Two Sorted Linked Lists.py
@@ -9,17 +9,22 @@ class Solution:
     #def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
     def mergeTwoLists(self, list1: [ListNode], list2: [ListNode]) -> [ListNode]:
 
+        #both lists are empty
         if list1 == None and list2 == None:
             return None
 
         finalList = ListNode()
         header = finalList
 
+        # if list1 is empty, return the entire list2
         if list1 == None:
             return list2
+
+        # if list2 is empty, return the entire list1
         if list2 == None:
             return list1
 
+        # iterate both lists until you reach the end in one of these
         while list1 != None and list2 != None:
 
             if list1.val > list2.val:
@@ -33,27 +38,21 @@ class Solution:
             finalList.next = newItem
             finalList = finalList.next
 
+
+            # if you reached the empty on list1, then 
             if list1 == None:
 
                 while list2.next != None :
 
                     finalList.val = list2.val
-                    finalList.next = ListNode()
-                    finalList = finalList.next
-                    list2 = list2.next
-
-                finalList.val = list2.val
+                    finalList.next = list2.next
 
             if list2 == None:
 
                 while list1.next != None:
 
                     finalList.val = list1.val
-                    finalList.next = ListNode()
-                    finalList = finalList.next
-                    list1 = list1.next
-
-                finalList.val = list1.val
+                    finalList.next = list1.next
                
         return header
             
