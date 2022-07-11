826cf8cdd58584794838eae829f07923ab4a283d
unknown <lucas.biff89@gmail.com>
2022-07-11 00:55:55 -0300

    accepted

--- H65. Valid Number.py
+++ H65. Valid Number.py
@@ -2,95 +2,139 @@
 
 class Solution:
     def isNumber(self, s: str) -> bool:
-        startIndex = 0
+
+        bNumberFound = False
         currentIndex = 0
-        bExponentFound = False
         bDotFound = False
-        strLength = len(s)
-
+        bExponentFound = False
+        strLen = len(s)
+    
+        # first character must be [number . + -]
         if not s[0].isnumeric():
-
-            if s[0] != '+' and s[0] != '-':
-                if s[0] != '.':
-                    return False
-                else:
-                    bDotFound = True
-                    if strLength == 1:
-                        return False
-                    startIndex = currentIndex = 1
-
-
-            else:
-                if strLength == 1:
-                    return False
-
-                startIndex = currentIndex = 1
-
-        for i in range(startIndex, strLength):
-            #print(i)
+            if strLen == 1:
+                return False
+            currentIndex = 1
+            if s[0] == '.':
+                bDotFound = True
+            elif s[0] != '+' and s[0] != '-':
+                return False     
+        
+        
+        # keep looking for numbers [0-9]
+        for i in range(currentIndex, strLen):
 
             if not s[i].isnumeric():
                 break
+            currentIndex = currentIndex + 1
+            bNumberFound = True
+
+        if currentIndex >= strLen:
+            return True
+
+        if s[currentIndex] != '.' and s[currentIndex] != 'e' and s[currentIndex] != 'E':
+            return False
+
+        # found a [. e E]
+        # if found . but already found one
+        if s[currentIndex] == '.':
+            if bDotFound:
+                return False
+            else:
+                bDotFound = True
+                currentIndex = currentIndex + 1
+
+                if currentIndex >= strLen:
+                    if bNumberFound:
+                        return True
+                    else:
+                        return False
+            
+
+        # if found [e E]
+        if s[currentIndex] == 'e' or s[currentIndex] == 'E':
+
+            # you can only stack [e E] if you already found a number
+            if not bNumberFound:
+                return False
+
+            bExponentFound = True
 
             currentIndex = currentIndex + 1
 
-        if currentIndex >= strLength:
-            return True
+            if currentIndex >= strLen:
+               return False
 
-
-        if s[currentIndex] != '.':
-            if s[currentIndex] != 'e' and s[currentIndex] != 'E':
-                return False
-            if bDotFound:
-                if s[currentIndex - 1] == '.':
-                    return False
-            bExponentFound = True
-        else:
-            if bDotFound == True:
-                return False
-
-            if strLength == 2 and not s[0].isnumeric():
-                return False
-
-        currentIndex = currentIndex + 1
-
-        if currentIndex >= strLength:
-            if bExponentFound == True:
-                return False
-
-            return True
-
-        if bExponentFound:
+            # after [e E] you could gave [+ -]
             if s[currentIndex] == '+' or s[currentIndex] == '-':
                 currentIndex = currentIndex + 1
 
-        if currentIndex >= strLength:
-            return True
+                if currentIndex >= strLen:
+                    return False    
 
-        for i in range(currentIndex, strLength):
+        # keep looking for numbers. this part could be after the first [.] found and could have not found and [e E] yet
+        for i in range(currentIndex, strLen):
 
             if not s[i].isnumeric():
-                if bExponentFound:
-                    if s[i] == '.':
-                        return False
                 break
-
             currentIndex = currentIndex + 1
+            bNumberFound = True
 
-        if currentIndex >= strLength:
+        if currentIndex >= strLen:
             return True
 
-        if not bExponentFound:
-            if s[currentIndex] != 'e' and s[currentIndex] != 'E':
+        # found [.] but at this point you already have found one
+        if s[currentIndex] == '.':
+            return False
+        
+        # by this point you can only have found [e E] 
+        if s[currentIndex] == 'e' or s[currentIndex] == 'E':
+
+            # you can only stack [e E] if you already found a number
+            if not bNumberFound:
                 return False
 
-        currentIndex = currentIndex + 1
+            # if you already found [e E] before 
+            if bExponentFound:
+                return False
 
-        for i in range(currentIndex, strLength):
+            else:
+                bExponentFound = True
+                currentIndex = currentIndex + 1
+
+                if currentIndex >= strLen:
+                    return False    
+
+                # after [e E] you could gave [+ -]
+                if s[currentIndex] == '+' or s[currentIndex] == '-':
+                    currentIndex = currentIndex + 1
+
+                    if currentIndex >= strLen:
+                        return False
+                    
+        else:
+            return False
+
+        # keep looking for numbers. here you are just looking for numbers after [e E] and nothing more
+        for i in range(currentIndex, strLen):
 
             if not s[i].isnumeric():
                 break
-
             currentIndex = currentIndex + 1
 
-        return True
+        # if you get a break by a non number, return invalid
+        if currentIndex >= strLen:
+            return True
+        else:
+            return False
+
+valid = ["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789", "-.3e6"]
+notValid = ["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53", '0.e', '1.e+', '.e+', '0e', '..2', '+.', '.+', '..', '+e', '-e', '10ee', '-29.2ee+19', '10e', '.', '.e1']
+
+print('============= VALID ============')
+
+for v in valid:
+    print(f'{Solution.isNumber(None, v)}: {v}')
+
+print('============= NOT VALID ============')
+for nv in notValid:
+    print(f'{Solution.isNumber(None, nv)}: {nv}')
